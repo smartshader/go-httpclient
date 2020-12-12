b43beffc26b028c10047ac47fe5e554ad9a56a79
Imad Jerra <imad.jerra@ariv.ca>
2020-12-12 11:07:50 -0500

    Dealing with request body

--- gohttp/client_core.go
+++ gohttp/client_core.go
@@ -1,22 +1,49 @@
 package gohttp
 
 import (
+	"bytes"
+	"encoding/json"
+	"encoding/xml"
 	"errors"
 	"net/http"
+	"strings"
 )
 
 func (c *client) do(method, url string, headers http.Header, body interface{}) (*http.Response, error) {
 	client := http.Client{}
 
-	request, err := http.NewRequest(method, url, nil)
+	fullHeaders := c.getRequestHeaders(headers)
+
+	requestBody, err := c.getRequestBody(fullHeaders.Get("Content-Type"), body)
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(requestBody))
 	if err != nil {
 		return nil, errors.New("unable to create a new request")
 	}
 
-	fullHeaders := c.getRequestHeaders(headers)
 	request.Header = fullHeaders
 
 	return client.Do(request)
+}
+
+func (c *client) getRequestBody(contentType string, body interface{}) ([]byte, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	switch strings.ToLower(contentType) {
+	case "application/json":
+		return json.Marshal(body)
+
+	case "application/xml":
+		return xml.Marshal(body)
+
+	default:
+		return json.Marshal(body)
+	}
 }
 
 func (c *client) getRequestHeaders(requestHeaders http.Header) http.Header {
