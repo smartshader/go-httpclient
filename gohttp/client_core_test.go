fbb5cc30ca68def7be9084a378d148a3efb73af7
Imad Jerra <imad.jerra@ariv.ca>
2020-12-12 14:17:53 -0500

    fix typo

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -10,7 +10,7 @@ func TestGetRequestHeaders(t *testing.T) {
 	commonHeaders := make(http.Header)
 	commonHeaders.Set("Content-Type", "application/json")
 	commonHeaders.Set("User-Agent", "cool-http-client")
-	httpClient.Headers = commonHeaders
+	httpClient.headers = commonHeaders
 
 	requestHeaders := make(http.Header)
 	requestHeaders.Set("X-Request-Id", "ABC-123")
